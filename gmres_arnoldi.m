fc5fbf6eee02597b981635eba208a3c7d49f53fa
Francesco Minnocci <ad17fmin@uwcad.it>
2023-06-20 13:39:02 +0200

    Add first draft

---
+++ gmres_arnoldi.m
@@ -0,0 +1,40 @@
+function [x, relres, it, resvec] = gmres_arnoldi(A, b, tol)
+    n = size(A, 1);
+    x = rand(n,1);
+    x = x/sum(x);
+    r = b - A * x;
+    beta = norm(r);
+    resvec = [beta];
+    it=0;
+    conv=0;
+
+    while conv == 0
+        it = it + 1;
+        Q = zeros(n, it + 1);
+        H = zeros(it + 1, it);
+        Q(:, 1) = r / beta;
+
+        for j = 1:it
+            w = A * Q(:, j);
+
+            for i = 1:j
+                H(i, j) = Q(:, i)' * w;
+                w = w - H(i, j) * Q(:, i);
+            end
+
+            H(j + 1, j) = norm(w);
+            Q(:, j + 1) = w / H(j + 1, j);
+        end
+
+        e1 = zeros(it + 1, 1);
+        e1(1) = beta;
+        y = H \ e1;
+        x = x + Q(:, 1:it) * y;
+        relres = abs(H(it + 1, it) * y(it)) / beta;
+        resvec = [resvec; relres];
+
+        if relres < tol
+            conv = 1;
+        end
+    end
+end
---
+++ page_rank.m
@@ -0,0 +1,35 @@
+% Carica la matrice
+load coAuthorsDBLP
+A = Problem.A;
+
+% Parametri
+gamma = [0.5, 0.7, 0.85, 0.99];
+tol = 1e-8;
+n = length(A);
+e = ones(n, 1);
+D = spdiags(A * e, 0, n, n);
+resvecs = cell(1, length(gamma));
+
+% Risoluzione del sistema lineare tramite GMRES
+for i = 1:length(gamma)
+    b = ((1-gamma(i))/n) * e;
+    M = eye(n) - gamma(i) * A * D^(-1);
+    tic;
+    [x, relres, it, resvec] = gmres_arnoldi(M, b, tol);
+    elapsed = toc;
+    resvecs{i} = resvec;
+    disp(['Numero di iterazioni per gamma = ', num2str(gamma(i)), ': ', num2str(it)]);
+    disp(['Tempo impiegato per gamma = ', num2str(gamma(i)), ': ', num2str(elapsed), ' secondi']);
+end
+
+% Plot dei residui per analizzare la convergenza al variare di gamma
+figure;
+for i = 1:length(gamma)
+    semilogy(resvecs{i}, 'o-', 'DisplayName', ['\gamma = ', num2str(gamma(i))]);
+    hold on;
+end
+grid on;
+xlabel('Numero di iterazioni');
+ylabel('Residui');
+title('Convergenza di GMRES al variare di gamma');
+legend('show');
