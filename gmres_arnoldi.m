51fe9562080e848ce48e0a19a5252ef522efff73
Francesco Minnocci <ad17fmin@uwcad.it>
2023-07-05 04:36:43 +0200

    sparsity isn't always best(?)

--- gmres_arnoldi.m
+++ gmres_arnoldi.m
@@ -1,39 +1,44 @@
-function [x, relres, it, resvec] = gmres_arnoldi(A, b, tol)
-    n = size(A, 1);
-    x = zeros(n,1);
-    r = b - A * x;
-    beta = norm(r);
-    resvec = [beta];
-    it=0;
-    conv=0;
+function [x, relres, it, resvec] = gmres_arnoldi(A, b, eps)
+	n = size(A, 1);
+	beta = norm(b);
+	v1 = b/beta;
+	resvec = [beta];
+	j = 0;
+	conv = 0;
+	itmax = 100;
+	V = zeros(n, itmax);
+	H = zeros(itmax);
+	V(:, 1) = v1;
 
-    while conv == 0
-        it = it + 1;
-        Q = zeros(n, it + 1);
-        H = zeros(it + 1, it);
-        Q(:, 1) = r / beta;
+	% Iterazione di Arnoldi
+	while (conv == 0 && j < itmax)
+		j = j + 1;
+		w = A * V(:, j);
 
-        for j = 1:it
-            w = A * Q(:, j);
+		% Ortogonalizzazione di A*v_j
+		for i = 1:j
+			H(i, j) = V(:, i)' * w;
+		end
+		w = w - V(:,1:j) * H(1:j, j);
 
-            for i = 1:j
-                H(i, j) = Q(:, i)' * w;
-                w = w - H(i, j) * Q(:, i);
-            end
+		H(j + 1, j) = norm(w);
+		% Normalizzazione del vettore ortogonalizzato
+		V(:, j + 1) = w / H(j + 1, j);
 
-            H(j + 1, j) = norm(w);
-            Q(:, j + 1) = w / H(j + 1, j);
-        end
+		% Risoluzione del problema ai minimi quadrati
+		e1 = zeros(j + 1, 1);
+		e1(1) = beta;
+		y = H(1:j+1,1:j) \ e1;
 
-        e1 = zeros(it + 1, 1);
-        e1(1) = beta;
-        y = H \ e1;
-        x = x + Q(:, 1:it) * y;
-        relres = abs(H(it + 1, it) * y(it)) / beta;
-        resvec = [resvec; relres];
+		% Calcolo del residuo
+		relres = norm(H(1:j+1,1:j) * y - e1);
+		resvec = [resvec; relres];
 
-        if relres < tol
-            conv = 1;
-        end
-    end
+		% Controllo della convergenza
+		if relres < eps
+			x = V(:,1:j) * y;
+			conv = 1;
+			it = j;
+		end
+	end
 end
--- page_rank.m
+++ page_rank.m
@@ -12,24 +12,24 @@ resvecs = cell(1, length(gamma));
 
 % Risoluzione del sistema lineare tramite GMRES
 for i = 1:length(gamma)
-    b = ((1-gamma(i))/n) * e;
-    M = eye(n) - gamma(i) * A * D^(-1);
-    tic;
-    [x, relres, it, resvec] = gmres_arnoldi(M, b, tol);
-    elapsed = toc;
-    resvecs{i} = resvec;
-    disp(['Numero di iterazioni per gamma = ', num2str(gamma(i)), ': ', num2str(it)]);
-    disp(['Tempo impiegato per gamma = ', num2str(gamma(i)), ': ', num2str(elapsed), ' secondi']);
+	b = ((1-gamma(i))/n) * e;
+	M = speye(n) - gamma(i) * A * D^(-1);
+	tic;
+	[x, relres, it, resvec] = gmres_arnoldi(M, b, tol);
+	elapsed = toc;
+	resvecs{i} = resvec;
+	disp(['gamma = ', num2str(gamma(i)), ': ', num2str(it), ' iterazioni effettuate in ', num2str(elapsed), ' secondi']);
 end
 
 % Plot dei residui per analizzare la convergenza al variare di gamma
 figure;
 for i = 1:length(gamma)
-    semilogy(resvecs{i}, 'o-', 'DisplayName', ['\gamma = ', num2str(gamma(i))]);
-    hold on;
+	semilogy(resvecs{i}, 'o-', 'DisplayName', ['\gamma = ', num2str(gamma(i))]);
+	hold on;
 end
 grid on;
 xlabel('Numero di iterazioni');
-ylabel('Residui');
+ylabel('Residuo');
 title('Convergenza di GMRES al variare di gamma');
 legend('show');
+print("convergenza.png");
